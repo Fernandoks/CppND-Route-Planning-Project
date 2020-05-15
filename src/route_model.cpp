87579c38f64ca7be5bc82b02cbd346908b6422f5
fernandoks <fernandosurjus@gmail.com>
2020-05-15 10:28:38 +0200

    Updates Review 15/05

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -1,6 +1,8 @@
 #include "route_model.h"
 #include <iostream>
 
+
+
 RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
     // Create RouteModel nodes.
     int counter = 0;
@@ -8,6 +10,7 @@ RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
         m_Nodes.emplace_back(Node(counter, this, node));
         counter++;
     }
+
     CreateNodeToRoadHashmap();
 }
 
